324612c1c74eefdad60ea15f9231f51f5c2ddf96
it21289316 <99386220+it21289316@users.noreply.github.com>
2022-02-10 14:09:53 +0000

    I have completed Question 3

--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,19 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+
+int n, count, sum = 0 ;
+
+printf("Enter integer : ");
+scanf("%d",&n);
+
+for (count = 1; count <= n ; count++)
+{
+  sum = sum + count;
+}
+
+printf("Sum of the integers from 1 to the entered number : %d",sum);
+
   return 0;
 }
 
